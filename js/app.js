b330a27566b32d3e545774ea73051469250359cd
Tadeu Zagallo <tadeuzagallo@gmail.com>
2014-12-15 14:47:31 -0200

    Fix collisions

--- js/app.js
+++ js/app.js
@@ -130,53 +130,36 @@
     }
   };
 
+  function findAnAngle(vx, vy) {
+    if (vy > 0) {
+      return Math.atan2(vy, vx);
+    } else {
+      return pi + pi + Math.atan2(vy, vx);
+    }
+  }
+
   function checkCollision (a, b) {
-    if (Math.abs(a.x - b.x) <= 1 && Math.abs(a.y - b.y) <= 1) {
-      var tanA = a.vy / a.vx;
-      var tanB = b.vy / b.vx;
-
-      var v1 = Math.sqrt(a.vx * a.vx + a.vy * a.vy);
-      var v2 = Math.sqrt(b.vx * b.vx + b.vy * b.vy);
-
-      var aangle, bangle;
-
-      if (tanA === 0) {
-        aangle = (a.vy > 0 ? 1 : -1) * pi_2;
-      } else {
-        aangle = Math.atan(tanA);
-      }
-
-      if (tanB === 0) {
-        bangle = (b.vy > 0 ? 1 : -1) * pi_2;
-      } else {
-        bangle = Math.atan(tanB);
-      }
-
-      if (a.vx < 0) {
-        aangle += pi;
-      }
-      if (b.vx < 0) {
-        bangle += pi;
-      }
-
+    var dx = a.x - b.x;
+    var dy = a.y - b.y;
+    if (dx * dx + dy * dy <= 1) {
       var phi;
-      var dx = a.vx - b.vx;
-      var dy = a.vy - b.vy;
+
       if (dx === 0) {
         phi = pi_2;
       } else {
         phi = Math.atan2(dy, dx);
       }
 
-      var v1xr = v1 * Math.cos(aangle - phi);
-      var v1yr = v1 * Math.sin(aangle - phi);
-      var v2xr = v2 * Math.cos(bangle - phi);
-      var v2yr = v2 * Math.sin(bangle - phi);
+      var aang = findAnAngle(a.vx, a.vy);
+      var bang = findAnAngle(b.vx, b.vy);
 
-      a.vx = v2xr;
-      a.vy = v1yr;
-      b.vx = v1xr;
-      b.vy = v2yr;
+      var v1 = Math.sqrt(a.vx * a.vx + a.vy * a.vy);
+      var v2 = Math.sqrt(b.vx * b.vx + b.vy * b.vy);
+
+      a.vx = v2 * Math.cos(bang - phi) * Math.cos(phi) + v1 * Math.sin(aang-phi) * Math.cos(phi+pi_2);
+      a.vy = v2 * Math.cos(bang - phi) * Math.sin(phi) + v1 * Math.sin(aang-phi) * Math.sin(phi+pi_2);
+      b.vx = v1 * Math.cos(aang - phi) * Math.cos(phi) + v2 * Math.sin(bang-phi) * Math.cos(phi+pi_2);
+      b.vy = v1 * Math.cos(aang - phi) * Math.sin(phi) + v2 * Math.sin(bang-phi) * Math.sin(phi+pi_2);
     }
   }
 
