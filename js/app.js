434e30553e94db8d5df9f7df8e634ac4e6232312
Tadeu Zagallo <tadeuzagallo@gmail.com>
2015-01-03 00:45:34 -0200

    WIP: Improving code

--- js/app.js
+++ js/app.js
@@ -10,16 +10,16 @@
     ammount: 3000,
     collision: true,
     image: window.location.hash === '#webcam' ? 'use your webcam' : 'the-bathers',
-    running: true,
+    running: false,
     zoom: 1,
     resolution: 4,
     isVideo: false
   };
 
   var maxParticles = 60000;
-  var incs = new Uint32Array([1391376, 463792, 198768, 86961, 33936, 13776, 4592, 1968, 861, 336, 112, 48, 21, 7, 3, 1]);
   var buffer = new ArrayBuffer(1<<21);
 
+  var size = 0;
   var pos = new Uint32Array(maxParticles);
   var lastxArray = new Float64Array(maxParticles);
   var lastyArray = new Float64Array(maxParticles);
@@ -28,7 +28,6 @@
   var vxArray = new Float64Array(maxParticles);
   var vyArray = new Float64Array(maxParticles);
   var colorsArray = new Uint32Array(maxParticles);
-  var size = 0;
   var particles = new Uint16Array(maxParticles);
 
   var width = 533;
@@ -46,11 +45,11 @@
 
   var data = [];
 
-  function fadeCanvas() {
+  var fadeCanvas = function () {
     context.fillRect(0, 0, width, height);
-  }
+  };
 
-  function renderImage(image, preventClear) {
+  var renderImage = function (image, preventClear) {
     var res = options.resolution;
     var id;
     var c;
@@ -81,9 +80,9 @@
     data = d;
     width = w;
     height = h;
-  }
+  };
 
-  function createParticle() {
+  var createParticle = function (size) {
     var x = Math.random() * width;
     var y = Math.random() * height;
 
@@ -94,56 +93,56 @@
     vxArray[size] = Math.random() - 0.5;
     vyArray[size] = Math.random() - 0.5;
     particles[size] = size;
-
-    size++;
-  }
-
-  function moveParticle(i) {
-    var lastX = xArray[i];
-    var lastY = yArray[i];
-
-    var vx = vxArray[i];
-    var vy = vyArray[i];
-
-    var x = lastX + vx * options.speed;
-    var y = lastY + vy * options.speed;
-
-    if (x >= width) {
-      x = width-1;
-      vx = -vx;
-    } else if (x < 0) {
-      x = 0;
-      vx = -vx;
-    }
-
-    if (y >= height) {
-      y = height-1;
-      vy = -vy;
-    } else if (y < 0) {
-      y = 0;
-      vy = -vy;
-    }
-
-    var _pos = (y>>0)*width+(x>>0);
-
-    lastxArray[i] = lastX;
-    lastyArray[i] = lastY;
-    xArray[i] = x;
-    yArray[i] = y;
-    vxArray[i] = vx;
-    vyArray[i] = vy;
-    colorsArray[i] = data[_pos];
-    pos[i] = _pos;
-  }
-
-  var scaleSystem = function (count) {
-    size = 0;
-    for (var i = 0; i < count; i++) {
-      createParticle();
-    }
   };
 
-  function findAnAngle(vx, vy) {
+  var moveParticle = (function (lastxArray, lastyArray, xArray, yArray, vxArray, vyArray, colorsArray, pos, options) {
+    return function moveParticle(i) {
+      var lastX = xArray[i];
+      var lastY = yArray[i];
+
+      var vx = vxArray[i];
+      var vy = vyArray[i];
+
+      var x = lastX + vx * options.speed;
+      var y = lastY + vy * options.speed;
+
+      if (x >= width) {
+        x = width-1;
+        vx = -vx;
+      } else if (x < 0) {
+        x = 0;
+        vx = -vx;
+      }
+
+      if (y >= height) {
+        y = height-1;
+        vy = -vy;
+      } else if (y < 0) {
+        y = 0;
+        vy = -vy;
+      }
+
+      var _pos = (y>>0)*width+(x>>0);
+
+      lastxArray[i] = lastX;
+      lastyArray[i] = lastY;
+      xArray[i] = x;
+      yArray[i] = y;
+      vxArray[i] = vx;
+      vyArray[i] = vy;
+      colorsArray[i] = data[_pos];
+      pos[i] = _pos;
+    }
+  })(lastxArray, lastyArray, xArray, yArray, vxArray, vyArray, colorsArray, pos, options);
+
+  var scaleSystem = function (count, width, height) {
+    for (var i = 0; i < count; i++) {
+      createParticle(i);
+    }
+    size = count;
+  };
+
+  var findAnAngle = function (vx, vy) {
     if (vy > 0) {
       return Math.atan2(vy, vx);
     } else if (vx === 0) {
@@ -153,103 +152,105 @@
     } else {
       return Math.PI + Math.PI + Math.atan2(vy, vx);
     }
-  }
+  };
 
-  function checkCollision (a, b) {
-    var ax = xArray[a];
-    var ay = yArray[a];
-    var bx = xArray[b];
-    var by = yArray[b];
+  var checkCollision = (function (xArray, yArray, vxArray, vyArray) {
+    return function checkCollision (a, b) {
+      var ax = xArray[a];
+      var ay = yArray[a];
+      var bx = xArray[b];
+      var by = yArray[b];
 
-    var dx = ax - bx;
-    var dy = ay - by;
-    if (dx * dx + dy * dy <= 1) {
-      var phi;
-      var avx = vxArray[a];
-      var avy = vyArray[a];
-      var bvx = vxArray[b];
-      var bvy = vyArray[b];
+      var dx = ax - bx;
+      var dy = ay - by;
+      if (dx * dx + dy * dy <= 1) {
+        var phi;
+        var avx = vxArray[a];
+        var avy = vyArray[a];
+        var bvx = vxArray[b];
+        var bvy = vyArray[b];
 
-      if (dx === 0) {
-        phi = Math.PI / 2;
+        if (dx === 0) {
+          phi = Math.PI / 2;
+        } else {
+          phi = Math.atan2(dy, dx);
+        }
+
+        var aang = findAnAngle(avx, avy);
+        var bang = findAnAngle(bvx, bvy);
+
+        var v1 = Math.sqrt(avx * avx + avy * avy);
+        var v2 = Math.sqrt(bvx * bvx + bvy * bvy);
+
+        var s_phi = Math.sin(phi);
+        var c_phi = Math.cos(phi);
+        var s_a_phi = Math.sin(aang-phi);
+        var s_b_phi = Math.sin(bang-phi);
+        var c_a_phi = Math.cos(aang-phi);
+        var c_b_phi = Math.cos(bang-phi);
+        var s_phi_pi2 = Math.sin(phi+Math.PI / 2);
+        var c_phi_pi2 = Math.cos(phi+Math.PI / 2);
+
+        vxArray[a] = v2 * c_b_phi * c_phi + v1 * s_a_phi * c_phi_pi2;
+        vyArray[a] = v2 * c_b_phi * s_phi + v1 * s_a_phi * s_phi_pi2;
+        vxArray[b] = v1 * c_a_phi * c_phi + v2 * s_b_phi * c_phi_pi2;
+        vyArray[b] = v1 * c_a_phi * s_phi + v2 * s_b_phi * s_phi_pi2;
+      }
+    };
+  })(xArray, yArray, vxArray, vyArray);
+
+  var loadImage = (function (canvas, options, video, preview) {
+    return function loadImage(image) {
+      if (image === 'use your webcam') {
+        var _running = options.running;
+
+        options.isVideo = true;
+        canvas.width = video.width;
+        canvas.height = video.height;
+        options.running = false;
+
+        preview.style.display = 'none';
+        video.style.display = '';
+
+        window.navigator.getUserMedia({ video: true }, function (localMediaStream) {
+          var video = document.querySelector('video');
+          video.src = window.URL.createObjectURL(localMediaStream);
+          video.onloadedmetadata = function () {
+            setTimeout(function () {
+              options.running = _running;
+            }, /firefox/i.test(navigator.userAgent) ? 1000 : 0);
+          };
+        }, function(err) { console.error(err); });
+
+        return;
       } else {
-        phi = Math.atan2(dy, dx);
+        options.isVideo = false;
+        preview.style.display = '';
+        video.style.display = 'none';
       }
 
-      var aang = findAnAngle(avx, avy);
-      var bang = findAnAngle(bvx, bvy);
+      function onload() {
+        renderImage(preview, false);
+        trailChanged(options.trail);
+      }
+      preview.src = 'images/' + image + '.jpg';
 
-      var v1 = Math.sqrt(avx * avx + avy * avy);
-      var v2 = Math.sqrt(bvx * bvx + bvy * bvy);
+      if (preview.complete) {
+        onload();
+      } else {
+        preview.onload = onload;
+      }
+    };
+  })(canvas, options, video, preview);
 
-      var s_phi = Math.sin(phi);
-      var c_phi = Math.cos(phi);
-      var s_a_phi = Math.sin(aang-phi);
-      var s_b_phi = Math.sin(bang-phi);
-      var c_a_phi = Math.cos(aang-phi);
-      var c_b_phi = Math.cos(bang-phi);
-      var s_phi_pi2 = Math.sin(phi+Math.PI / 2);
-      var c_phi_pi2 = Math.cos(phi+Math.PI / 2);
-
-      vxArray[a] = v2 * c_b_phi * c_phi + v1 * s_a_phi * c_phi_pi2;
-      vyArray[a] = v2 * c_b_phi * s_phi + v1 * s_a_phi * s_phi_pi2;
-      vxArray[b] = v1 * c_a_phi * c_phi + v2 * s_b_phi * c_phi_pi2;
-      vyArray[b] = v1 * c_a_phi * s_phi + v2 * s_b_phi * s_phi_pi2;
+  var trailChanged = (function (context) {
+    return function trailChanged(trail) {
+      context.fillStyle = 'rgba(0,0,0,'+trail+')';
     }
-  }
+  })(context);
 
-  function loadImage(image) {
-    if (image === 'use your webcam') {
-      var _running = options.running;
-
-      options.isVideo = true;
-      canvas.width = video.width;
-      canvas.height = video.height;
-      options.running = false;
-
-      preview.style.display = 'none';
-      video.style.display = '';
-
-      navigator.getUserMedia = ( navigator.getUserMedia ||
-                                navigator.webkitGetUserMedia ||
-                                navigator.mozGetUserMedia ||
-                                navigator.msGetUserMedia);
-      navigator.getUserMedia({ video: true }, function (localMediaStream) {
-        var video = document.querySelector('video');
-        video.src = window.URL.createObjectURL(localMediaStream);
-        video.onloadedmetadata = function () {
-          setTimeout(function () {
-            options.running = _running;
-          }, /firefox/i.test(navigator.userAgent) ? 1000 : 0);
-        };
-      }, function(err) { console.error(err); });
-
-      return;
-    } else {
-      options.isVideo = false;
-      preview.style.display = '';
-      video.style.display = 'none';
-    }
-
-    function onload() {
-      renderImage(preview, false);
-      trailChanged(options.trail);
-    }
-    preview.src = 'images/' + image + '.jpg';
-
-    if (preview.complete) {
-      onload();
-    } else {
-      preview.onload = onload;
-    }
-  }
-
-  function trailChanged(trail) {
-    context.fillStyle = 'rgba(0,0,0,'+trail+')';
-  }
-
-  var imageSelect = (function () {
-    var gui = new dat.GUI(); // jshint ignore: line
+  var imageSelect = (function (dat, options, video, preview) {
+    var gui = new dat.GUI();
 
     gui.add(options, 'speed', 1, 10);
 
@@ -304,114 +305,118 @@
     });
 
     return listeners.image;
-  })();
+  })(window.dat, options, video, preview);
 
-  var stats = (function () {
-    var stats = new Stats(); // jshint ignore: line
+  var stats = (function (State) {
+    var stats = new Stats();
     stats.domElement.style.position = 'fixed';
     stats.domElement.style.right = '0px';
     stats.domElement.style.bottom = '0px';
     document.body.appendChild(stats.domElement);
 
     return stats;
-  })();
+  })(window.Stats);
+
 
   trailChanged(options.trail);
   scaleSystem(options.ammount);
   loadImage(options.image);
 
 
-  var count = new Uint16Array(1<<16);
-  var draws = new Uint16Array(maxParticles);
-  function render() {
-    var res = options.resolution;
-    var ctx = context;
-    var h, i, j, k, l = size;
-    var _v, v, p;
-    var color, r, g, b;
+  var render = (function (ctx, options, particles, pos, colorsArray, lastxArray, lastyArray, xArray, yArray) {
+    var incs = new Uint32Array([1391376, 463792, 198768, 86961, 33936, 13776, 4592, 1968, 861, 336, 112, 48, 21, 7, 3, 1]);
+    var count = new Uint16Array(1<<16);
+    var draws = new Uint16Array(maxParticles);
 
-    if (options.running) {
-      stats.begin();
+    return function render() {
+      var res = options.resolution;
+      var h, i, j, k, l = size;
+      var _v, v, p;
+      var color, r, g, b;
 
-      if (options.isVideo) {
-        renderImage(video, true);
-        trailChanged(options.trail);
-      }
+      if (options.running) {
+        stats.begin();
 
-      fadeCanvas();
+        if (options.isVideo) {
+          renderImage(video, true);
+          trailChanged(options.trail);
+        }
 
-      if (options.collision) {
-        for (k = 0; k < 16; k++) {
-          for (h = incs[k], i = h; i < l; i++) {
-            _v = particles[i];
-            v = pos[_v];
-            j = i;
+        fadeCanvas();
 
-            while (j >= h && pos[particles[j - h]] > v) {
-              particles[j] = particles[j - h];
-              j -= h;
+        if (options.collision) {
+          for (k = 0; k < 16; k++) {
+            for (h = incs[k], i = h; i < l; i++) {
+              _v = particles[i];
+              v = pos[_v];
+              j = i;
+
+              while (j >= h && pos[particles[j - h]] > v) {
+                particles[j] = particles[j - h];
+                j -= h;
+              }
+
+              particles[j] = _v;
             }
-
-            particles[j] = _v;
           }
         }
-      }
 
-      for (i = 1; i < 1<<16; i++) {
-        count[i] = 0;
-      }
-      for (i = 0; i < l; i++) {
-        p = particles[i];
-        if (i+1 < l && options.collision) {
-          checkCollision(p, particles[i+1]);
+        for (i = 1; i < 1<<16; i++) {
+          count[i] = 0;
+        }
+        for (i = 0; i < l; i++) {
+          p = particles[i];
+          if (i+1 < l && options.collision) {
+            checkCollision(p, particles[i+1]);
+          }
+
+          moveParticle(p);
+          color = colorsArray[p];
+
+          count[color]++;
         }
 
-        moveParticle(p);
-        color = colorsArray[p];
-
-        count[color]++;
-      }
-
-      for (i = 1; i < 1<<16; i++) {
-        count[i] += count[i-1];
-      }
-
-      for (i = 0; i < l; i++) {
-        draws[--count[colorsArray[particles[i]]]] = particles[i];
-      }
-
-      var prevColor = -1;
-      for (i = 0; i < l; i++) {
-        p = draws[i];
-        color = colorsArray[p];
-
-        if (color !== prevColor) {
-          ctx.stroke();
-
-          r = color >> 10;
-          g = (color >> 5) & 31;
-          b = color & 31;
-
-          ctx.strokeStyle = 'rgb('+(r << res)+', '+(g << res)+', '+(b << res)+')';
-          ctx.beginPath();
-          prevColor = color;
+        for (i = 1; i < 1<<16; i++) {
+          count[i] += count[i-1];
         }
 
-        var lastX = lastxArray[p];
-        var lastY = lastyArray[p];
-        var x = xArray[p];
-        var y = yArray[p];
+        for (i = 0; i < l; i++) {
+          draws[--count[colorsArray[particles[i]]]] = particles[i];
+        }
 
-        ctx.moveTo(lastX, lastY);
-        ctx.lineTo(x, y);
+        var prevColor = -1;
+        for (i = 0; i < l; i++) {
+          p = draws[i];
+          color = colorsArray[p];
+
+          if (color !== prevColor) {
+            ctx.stroke();
+
+            r = color >> 10;
+            g = (color >> 5) & 31;
+            b = color & 31;
+
+            ctx.strokeStyle = 'rgb('+(r << res)+', '+(g << res)+', '+(b << res)+')';
+            ctx.beginPath();
+            prevColor = color;
+          }
+
+          var lastX = lastxArray[p];
+          var lastY = lastyArray[p];
+          var x = xArray[p];
+          var y = yArray[p];
+
+          ctx.moveTo(lastX, lastY);
+          ctx.lineTo(x, y);
+        }
+
+        ctx.stroke();
+        stats.end();
       }
 
-      ctx.stroke();
-      stats.end();
-    }
-
-    window.requestAnimationFrame(render);
-  }
+      window.requestAnimationFrame(render);
+    };
+  })(context, options, particles, pos, colorsArray, lastxArray, lastyArray, xArray, yArray);
 
   webcam.onclick = function (e) {
     e.preventDefault();
@@ -420,5 +425,9 @@
     imageSelect.updateDisplay();
   };
 
+  window.navigator.getUserMedia = ( navigator.getUserMedia ||
+                            navigator.webkitGetUserMedia ||
+                            navigator.mozGetUserMedia ||
+                            navigator.msGetUserMedia);
   window.requestAnimationFrame(render);
 })(window, document);
